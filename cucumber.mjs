03a93cfb3c8b09add2d4e4b1a577ec35ee808ff1
David Goss <david@davidgoss.co>
2022-11-25 09:29:15 +0000

    get it working

--- cucumber.mjs
+++ cucumber.mjs
@@ -2,6 +2,5 @@ export default {
   parallel: 2,
   paths: ['tests/features/**/*.{feature,feature.md}'], // this could be cucumber-js somewhere-else/**/*.feature
   format: ['html:cucumber-report.html'],
-  require: ['tests/steps/**/*.ts'],
-  requireModule: ['@babel/register'],
+  require: ['tests/bootstrap.js', 'tests/steps/**/*.ts']
 }
